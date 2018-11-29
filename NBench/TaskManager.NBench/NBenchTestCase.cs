323dcd40e3a5844cffe0bde58d1251055a6647c8
venkatdev22 <venkat4fz@gmail.com>
2018-11-29 03:36:10 -0800

    Signed-off-by: venkatdev22 <venkat4fz@gmail.com>

--- NBench/TaskManager.NBench/NBenchTestCase.cs
+++ NBench/TaskManager.NBench/NBenchTestCase.cs
@@ -1,33 +1,29 @@
-﻿using NBench;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Json;
 using System.Text;
-using System.Net;
+using NBench;
 using Task.API.Controllers;
 using TaskManager.Model;
-using System.Collections.Generic;
+
 
 namespace TaskManager.NBench
 {
     public class NBenchTestCase : PerformanceTestStuite<NBenchTestCase>
     {
         #region Variables
-        TaskController objTaskManagerController = null;
-        TASK_DETAILS objGET_TASK_DETAILS_Result = null;
-
+        TaskController taskMgrController = null;
         private const int AcceptableMinAddThroughput = 500;
         #endregion
 
         [PerfSetup]
         public void SetUp(BenchmarkContext context)
         {
-            objTaskManagerController = new TaskController();
-            objGET_TASK_DETAILS_Result = new TASK_DETAILS();
+            taskMgrController = new TaskController();
         }
 
-        #region GetParentTask
         /// <summary>
         /// To get Parent task details
         /// </summary>
@@ -36,7 +32,7 @@ namespace TaskManager.NBench
         [ElapsedTimeAssertion(MaxTimeMilliseconds = 600000)]
         public void GetParentTask()
         {
-            var response = objTaskManagerController.Get();
+            var response = taskMgrController.Get();
             if (response != null)
             {
                 var content = response.Content;
@@ -44,18 +40,16 @@ namespace TaskManager.NBench
                 var vCount = vlsit?.Count();
             }
         }
-        #endregion
 
-        #region GetTaskDetails
         /// <summary>
-        /// Method for getting the task details from BL and send back to HTML
+        /// Get task details
         /// </summary>
         /// <returns></returns>
         [PerfBenchmark(RunMode = RunMode.Iterations, NumberOfIterations = 500, TestMode = TestMode.Test, SkipWarmups = true)]
         [ElapsedTimeAssertion(MaxTimeMilliseconds = 600000)]
         public void GetTaskDetails()
         {
-            var response = objTaskManagerController.GetTasks();
+            var response = taskMgrController.GetTasks();
             if (response != null)
             {
                 var content = response.Content;
@@ -63,77 +57,43 @@ namespace TaskManager.NBench
                 var vCount = vlsit?.Count();
             }
         }
-        #endregion
 
-        #region InsertTaskDetails
         /// <summary>
-        /// Insert the Task details which user entered
+        /// Insert the Task details 
         /// </summary>
-        /// <param name="objGET_TASK_DETAILS_Result"></param>
         /// <returns></returns>
         [PerfBenchmark(RunMode = RunMode.Iterations, NumberOfIterations = 500, TestMode = TestMode.Test, SkipWarmups = true)]
         [ElapsedTimeAssertion(MaxTimeMilliseconds = 600000)]
         public void InsertTaskDetails()
         {
-            objGET_TASK_DETAILS_Result = new TASK_DETAILS();
-
-            #region Insert new records
-
             var addRequest = new Model.TASK_DETAILS()
             {
                 Task = "SecondTask",
-                Start_Date = DateTime.Now,
-                End_Date = DateTime.Now.AddMonths(2),
+                Start_Date = DateTime.Now.ToString(),
+                End_Date = DateTime.Now.AddMonths(2).ToString(),
                 Priority = 3
             };
-
-            #endregion
-
-            #region Update records
-
-            //objGET_TASK_DETAILS_Result.Task_ID = 4;
-            //objGET_TASK_DETAILS_Result.Parent_ID = 1;
-            //objGET_TASK_DETAILS_Result.Task = "Update new task";
-            //objGET_TASK_DETAILS_Result.Start_Date = DateTime.Now;
-            //objGET_TASK_DETAILS_Result.End_Date = null;
-            //objGET_TASK_DETAILS_Result.Priority = 16;
-
-            #endregion
-
-            var vlsit = objTaskManagerController.Post(addRequest);
-
+            var vlsit = taskMgrController.Post(addRequest);
         }
-        #endregion
-
-        #region UpdateEndTask
+        
         /// <summary>
         /// Update the End task 
-        /// </summary>
-        /// <param name="objGET_TASK_DETAILS_Result"></param>
-        /// <returns></returns>
+        /// </summary        
         [PerfBenchmark(RunMode = RunMode.Iterations, NumberOfIterations = 500, TestMode = TestMode.Test, SkipWarmups = true)]
         [ElapsedTimeAssertion(MaxTimeMilliseconds = 600000)]
         public void UpdateEndTask()
         {
-            objGET_TASK_DETAILS_Result = new TASK_DETAILS();
-
-            #region Update records
-
-            objGET_TASK_DETAILS_Result.Task_ID = "38FE07D7-4A67-4AC5-AAB1-73F7D528ABAC";
-            objGET_TASK_DETAILS_Result.End_Date = DateTime.Now;
-
-            #endregion
-
-            var vlsit = objTaskManagerController.Put(objGET_TASK_DETAILS_Result);
+            var request = new TASK_DETAILS();
+            request.Task_ID = "38FE07D7-4A67-4AC5-AAB1-73F7D528ABAC";
+            request.End_Date = DateTime.Now.ToString();
+            var vlsit = taskMgrController.Put(request);
         }
-        #endregion 
+        
 
         [PerfCleanup]
         public void Cleanup(BenchmarkContext context)
         {
-             objTaskManagerController = null;
-             objGET_TASK_DETAILS_Result = null;
-
+             taskMgrController = null;
         }
     }
 
